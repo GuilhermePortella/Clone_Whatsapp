8926eeaafd82f836c274ed518479f419c0ad5aa3
guilhermeportella <guilhermeportella2@gmail.com>
2022-01-30 01:38:28 -0300

    Finalizacao do front e config back

--- src/App.js
+++ src/App.js
@@ -1,82 +1,105 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+import Api from './Api';
+
+import ChatListItem from './components/ChatListItem';
+import ChatIntro from './components/ChatIntro';
+import ChatWindow from './components/ChatWindow';
+import NewChat from './components/NewChat';
+import Login from './components/Login';
 
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchIcon from '@material-ui/icons/Search';
 
-import ChatListItem from './components/ChatListItem';
-import ChatIntro from './components/ChatIntro';
-import ChatWindow from './components/ChatWindow';
-
 export default () => {
-  const[chatlist, setChatlist] = useState([
-    {chatId: 1, title:'Fulano de tal 1', Image: 'https://www.w3schools.com/howto/img_avatar2.png'},
-    {chatId: 2, title:'Fulano de tal 2', Image: 'https://www.w3schools.com/howto/img_avatar2.png'},
-    {chatId: 3, title:'Fulano de tal 3', Image: 'https://www.w3schools.com/howto/img_avatar2.png'},
-    {chatId: 4, title:'Fulano de tal 4', Image: 'https://www.w3schools.com/howto/img_avatar2.png'},
-    {chatId: 5, title:'Fulano de tal 5', Image: 'https://www.w3schools.com/howto/img_avatar2.png'}
-  ]);
-  const[activeChat, setActiveChat] = useState({});
 
-  return (
-    <div className = "app-window">
-        <div className = "sidebar">
+    const [chatlist, setChatList] = useState([]);
+    const [activeChat, setActiveChat] = useState({});
+    const [user, setUser] = useState(null);
+    const [showNewChat, setShowNewChat] = useState(false);
 
-            <header>
-              
-                <img className = "header--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt=""/>
+    useEffect(()=>{
+        if(user !== null) {
+            let unsub = Api.onChatList(user.id, setChatList);
+            return unsub;
+        }
+    }, [user]);
 
-                  <div className = "header--buttons">
+    const handleNewChat = () => {
+        setShowNewChat(true);
+    }
 
-                      <div className = "header--btn">
-                        <DonutLargeIcon style={{color:'#919191'}} />
-                      </div>
+    const handleLoginData = async (u) => {
+        let newUser = {
+            id: u.uid,
+            name: u.displayName,
+            avatar: u.photoURL
+        };
+        await Api.addUser(newUser);
+        setUser(newUser);
+    }
 
-                      <div className = "header--btn">
-                        <ChatIcon style = {{color:'#919191'}}/>
-                      </div>
+    if(user === null) {
+        return (<Login onReceive={handleLoginData} />);
+    }
 
-                      <div className = "header--btn">
-                        <MoreVertIcon style = {{color:'#919191'}}/>
-                      </div>
+    return (
+        <div className="app-window">
+            <div className="sidebar">
+                <NewChat
+                    chatlist={chatlist}
+                    user={user}
+                    show={showNewChat}
+                    setShow={setShowNewChat}
+                />
+                <header>
+                    <img className="header--avatar" src={user.avatar} alt="" />
+                    <div className="header--buttons">
+                        <div className="header--btn">
+                            <DonutLargeIcon style={{color: '#919191'}} />
+                        </div>
+                        <div onClick={handleNewChat} className="header--btn">
+                            <ChatIcon style={{color: '#919191'}} />
+                        </div>
+                        <div className="header--btn">
+                            <MoreVertIcon style={{color: '#919191'}} />
+                        </div>
+                    </div>
+                </header>
 
-                  </div>
+                <div className="search">
+                    <div className="search--input">
+                        <SearchIcon fontSize="small" style={{color: '#919191'}} />
+                        <input type="search" placeholder="Procurar ou começar uma nova conversa" />
+                    </div>
+                </div>
 
-            </header>
-
-            <div className = "search">
-              
-                <div className = "search--input">
-                   <SearchIcon fontsize='small' style = {{color:'#919191'}}/>
-                   <input type="search" placeholder="Procurar ou começar uma nova conversa"/>
+                <div className="chatlist">
+                    {chatlist.map((item, key)=>(
+                        <ChatListItem
+                            key={key}
+                            data={item}
+                            active={activeChat.chatId === chatlist[key].chatId}
+                            onClick={()=>setActiveChat(chatlist[key])}
+                        />
+                    ))}
                 </div>
 
             </div>
-
-            <div className = "chatlist">
-                {chatlist.map((item, key)=>(
-                  <ChatListItem
-                    key = {key}
-                    data={item}
-                    active={activeChat.chatId === (chatlist[key]).chatId}
-                    onClick={()=>setActiveChat(chatlist[key])}
-                  />
-                ))}
-
+            <div className="contentarea">
+                {activeChat.chatId !== undefined &&
+                    <ChatWindow
+                        user={user}
+                        data={activeChat}
+                    />
+                }
+                {activeChat.chatId === undefined &&
+                    <ChatIntro />
+                }
             </div>
-
         </div>
-
-        <div className = "contentarea">
-            {activeChat.chatId !== undefined &&
-              <ChatWindow/>
-            }
-            {activeChat.chatId === undefined &&
-              <ChatIntro/>
-            }
-        </div>
-    </div>
-  );
+    );
 }
--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,7 +1,10 @@
-import React, {useState} from 'react';
-import './ChatWindow.css';
+import React, { useState, useEffect, useRef } from 'react';
 import EmojiPicker from 'emoji-picker-react';
+import './ChatWindow.css';
 
+import Api from '../Api';
+
+import MessageItem from './MessageItem';
 
 import SearchIcon from '@material-ui/icons/Search';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
@@ -11,19 +14,33 @@ import CloseIcon from '@material-ui/icons/Close';
 import SendIcon from '@material-ui/icons/Send';
 import MicIcon from '@material-ui/icons/Mic';
 
-export default () =>{
+export default ({user, data}) => {
+
+    const body = useRef();
 
     let recognition = null;
     let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-    if(SpeechRecognition !== undefined){
+    if(SpeechRecognition !== undefined) {
         recognition = new SpeechRecognition();
-
     }
 
     const [emojiOpen, setEmojiOpen] = useState(false);
     const [text, setText] = useState('');
     const [listening, setListening] = useState(false);
-    const [list, setList] = useState([{},{},{}]);
+    const [list, setList] = useState([]);
+    const [users, setUsers] = useState([]);
+
+    useEffect(()=>{
+        setList([]);
+        let unsub = Api.onChatContent(data.chatId, setList, setUsers);
+        return unsub;
+    }, [data.chatId]);
+
+    useEffect(()=>{
+        if(body.current.scrollHeight > body.current.offsetHeight) {
+            body.current.scrollTop = body.current.scrollHeight - body.current.offsetHeight;
+        }
+    }, [list]);
 
     const handleEmojiClick = (e, emojiObject) => {
         setText( text + emojiObject.emoji );
@@ -32,13 +49,11 @@ export default () =>{
     const handleOpenEmoji = () => {
         setEmojiOpen(true);
     }
-
     const handleCloseEmoji = () => {
         setEmojiOpen(false);
     }
-
     const handleMicClick = () => {
-        if(recognition !== null){
+        if(recognition !== null) {
 
             recognition.onstart = () => {
                 setListening(true);
@@ -47,115 +62,115 @@ export default () =>{
                 setListening(false);
             }
             recognition.onresult = (e) => {
-                setText(e.results[0][0].transcript);
+                setText( e.results[0][0].transcript );
             }
 
             recognition.start();
-
         }
+    }
 
+    const handleInputKeyUp = (e) => {
+        if(e.keyCode == 13) {
+            handleSendClick();
+        }
     }
     const handleSendClick = () => {
-
+        if(text !== '') {
+            Api.sendMessage(data, user.id, 'text', text, users);
+            setText('');
+            setEmojiOpen(false);
+        }
     }
 
-
-    return(
+    return (
         <div className="chatWindow">
             <div className="chatWindow--header">
 
                 <div className="chatWindow--headerinfo">
-                    <img className="chatWindow--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt=""/>
-                    <div className="chatWindow--name">Guilherme Portella</div>
+                    <img className="chatWindow--avatar" src={data.image} alt="" />
+                    <div className="chatWindow--name">{data.title}</div>
                 </div>
 
                 <div className="chatWindow--headerbuttons">
-                    
-                    <div className ="chatWindow--btn">
-                        <SearchIcon style={{color: '#919191'}}/>
+
+                    <div className="chatWindow--btn">
+                        <SearchIcon style={{color: '#919191'}} />
+                    </div>
+                    <div className="chatWindow--btn">
+                        <AttachFileIcon style={{color: '#919191'}} />
+                    </div>
+                    <div className="chatWindow--btn">
+                        <MoreVertIcon style={{color: '#919191'}} />
                     </div>
 
-                    <div className ="chatWindow--btn">
-                        <AttachFileIcon style={{color: '#919191'}}/>
-                    </div>
-
-                    <div className ="chatWindow--btn">
-                        <MoreVertIcon style={{color: '#919191'}}/>
-                    </div>         
                 </div>
 
             </div>
-            <div className="chatWindow--body">
+            <div ref={body} className="chatWindow--body">
                 {list.map((item, key)=>(
-                    <messageItem
+                    <MessageItem
                         key={key}
                         data={item}
+                        user={user}
                     />
                 ))}
             </div>
-
-
+            
             <div
             className="chatWindow--emojiarea"
-            style={{height: emojiOpen ? '200px' : '0px' }}>
-
-                <EmojiPicker style = {{ width: '100%' }}
+            style={{height: emojiOpen ? '200px' : '0px'}}>
+                <EmojiPicker
                     onEmojiClick={handleEmojiClick}
                     disableSearchBar
                     disableSkinTonePicker
                 />
             </div>
 
-
-            <div className="chatWindow--fooder">
+            <div className="chatWindow--footer">
 
                 <div className="chatWindow--pre">
-
-                    <div 
-                        className ="chatWindow--btn"
+                    
+                    <div
+                        className="chatWindow--btn"
                         onClick={handleCloseEmoji}
-                        style={{width: emojiOpen?40:0}}    
+                        style={{width: emojiOpen?40:0}}
                     >
-                        <CloseIcon style={{color: '#919191'}}/>
+                        <CloseIcon style={{color: '#919191'}} />
                     </div>
 
-
-                    <div 
-                        className ="chatWindow--btn"
+                    <div
+                        className="chatWindow--btn"
                         onClick={handleOpenEmoji}
                     >
-                        <InsertEmoticonIcon style={{color: emojiOpen?'#009688':'#919191'}}/>
+                        <InsertEmoticonIcon style={{color: emojiOpen?'#009688':'#919191'}} />
                     </div>
-                
 
                 </div>
-                
                 <div className="chatWindow--inputarea">
-                    <input 
-                        className="chatWindow--input" 
-                        type="text" 
+                    <input
+                        className="chatWindow--input"
+                        type="text"
                         placeholder="Digite uma mensagem"
                         value={text}
                         onChange={e=>setText(e.target.value)}
+                        onKeyUp={handleInputKeyUp}
                     />
                 </div>
-
                 <div className="chatWindow--pos">
-
+                    
                     {text === '' &&
-                    <div onClick={handleMicClick} className ="chatWindow--btn">
-                        <MicIcon style={{color: listening ? '#126ece' : '#919191'}}/>
-                    </div>
+                        <div onClick={handleMicClick} className="chatWindow--btn">
+                            <MicIcon style={{color: listening ? '#126ECE' : '#919191'}} />
+                        </div>
                     }
-
                     {text !== '' &&
-                    <div onClick={handleSendClick} className ="chatWindow--btn">
-                        <SendIcon style={{color: '#919191'}}/>
-                    </div>
+                        <div onClick={handleSendClick} className="chatWindow--btn">
+                            <SendIcon style={{color: '#919191'}} />
+                        </div>
                     }
-
 
                 </div>
+
             </div>
         </div>
     );
